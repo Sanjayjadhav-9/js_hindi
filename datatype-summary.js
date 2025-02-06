3db83badefa3fe711e9cb39a92b979d36c814d1d
SANJAY GUGULOTH <74203629+Sanjayjadhav-9@users.noreply.github.com>
2025-02-06 15:32:40 +0000

    datatype summary

---
+++ datatype-summary.js
@@ -0,0 +1,29 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null // object
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 3546846549841846556n // bigInt
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "arjun", "krishna"];
+let myObj = {
+    name: "sanjay",
+    age: 25
+}
+
+const myFunc = function(){
+    console.log("Hello World!");
+}
