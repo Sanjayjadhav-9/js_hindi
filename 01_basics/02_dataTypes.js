ca4c6d59f1404437e612f6f7ce37716485aed635
SANJAY GUGULOTH <74203629+Sanjayjadhav-9@users.noreply.github.com>
2025-02-06 12:25:34 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,5 +5,23 @@
 console.log( 3 
     + 3); // code readability should be high.
 
-console.log("Sanjay");
+// console.log("Sanjay");
+
+let name = "sanjay"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 ^ 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
 
