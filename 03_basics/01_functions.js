74100b59b42c4722ee068d25d2795058c08f6751
SANJAY GUGULOTH <74203629+Sanjayjadhav-9@users.noreply.github.com>
2025-02-08 22:20:21 +0000

    functions done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+function sayMyName() {
+    console.log("S");
+    console.log("A");
+    console.log("N");
+    console.log("J");
+    console.log("A");
+    console.log("Y");
+}
+
+// sayMyName()
+
+function addTwoNumbers(num1, num2){
+    // console.log(num1 + num2);
+    return num1 + num2;
+    console.log("sanjay"); // function doesn't do anything after return key word 
+}
+
+const result = addTwoNumbers(3, 5)
+// console.log(result);
